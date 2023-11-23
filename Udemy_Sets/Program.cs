42623edbb263248c354ca128e4e803b50ee6fea4
Felipe <lipesmaffini@gmail.com>
2023-11-22 22:09:35 -0400

    Final implementation

--- Udemy_Sets/Program.cs
+++ Udemy_Sets/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Udemy_Sets.Entities;
+
+Console.Write("How many students for course A? ");
+int qtt = int.Parse(Console.ReadLine());
+
+HashSet<StudentRecord> students = new HashSet<StudentRecord>();
+for (int i = 0; i < qtt; i++)
+{
+    students.Add(new StudentRecord(int.Parse(Console.ReadLine())));
+}
+
+Console.Write("How many students for course B? ");
+qtt = int.Parse(Console.ReadLine());
+for (int i = 0; i < qtt; i++)
+{
+    students.Add(new StudentRecord(int.Parse(Console.ReadLine())));
+}
+
+Console.Write("How many students for course C? ");
+qtt = int.Parse(Console.ReadLine());
+for (int i = 0; i < qtt; i++)
+{
+    students.Add(new StudentRecord(int.Parse(Console.ReadLine())));
+}
+
+Console.WriteLine('\n'+"Total Students:"+students.Count());
